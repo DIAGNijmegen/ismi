97d7266b2797325ffe3f9c1e148ac14142a528ec
Joeran Bosma <joeran@bosma.co>
2022-12-15 11:31:15 +0100

    Remove pydicom dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         long_description = fh.read()
 
     setuptools.setup(
-        version='0.1.0',
+        version='0.1.1',
         author_email='Joeran.Bosma@radboudumc.nl',
         long_description=long_description,
         long_description_content_type="text/markdown",
